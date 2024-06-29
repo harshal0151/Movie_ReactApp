4d74da6e938cf52cac25ca9a699951aef19a788c
harshal0151 <patilharshal0151@gmail.com>
2024-06-29 17:29:36 +0530

    header search fun bug solve

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { CiSearch } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
-// import Logo from "../assets/logo.png"
-
 
 function Header() {
-  return (
-   <header className=' backdrop-blur-sm w-full fixed z-50 top-0 '>
-     <div className='flex items-center justify-between py-5 px-[8rem]   '>
-        <div className="logo text-orange-400 text-2xl font-bold" >
-            <Link to="/"><span >MOVIES.com</span></Link>
-        </div>
-        <div className="navlinks flex gap-6 font-semibold">
-            <Link to= "./movies"><li>Movies</li></Link>
-            <Link to= "/tv"><li>TV Shows</li></Link>
-            <Link className='icons'><li><CiSearch/></li></Link>
-        </div>
-    </div>
-   </header>
-  )
-}   
+  const [showInput, setShowInput] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-export default Header
+  const toggleInput = () => {
+    setShowInput(prevState => !prevState);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate("/search/" + searchTerm);
+  }
+
+  return (
+    <header className='backdrop-blur-sm w-full fixed z-50 top-0'>
+      <div className='flex items-center justify-between py-5 px-[8rem]'>
+        <div className="logo text-orange-400 text-2xl font-bold">
+          <Link to="/"><span>MOVIES.com</span></Link>
+        </div>
+        <div className="navlinks flex items-center gap-6 font-semibold">
+          <Link to="/movies"><li>Movies</li></Link>
+          <Link to="/tv"><li>TV Shows</li></Link>
+          <div className='flex items-center'>
+            {showInput && (
+              <form onSubmit={handleSubmit} className="flex items-center">
+                <input 
+                  type="text" 
+                  className="search-input bg-transparent text-white border-b px-2 outline-none"
+                  placeholder="Search..." 
+                  value={searchTerm} 
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </form>
+            )}
+            <CiSearch className='icons cursor-pointer text-white text-xl hover:text-sky-700' onClick={toggleInput} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
