352e473d44e2303ca8f01903cfb9582ea3b4f9a6
harshal0151 <patilharshal0151@gmail.com>
2024-07-14 19:20:18 +0530

    bug Fixed

--- src/slice.js
+++ src/slice.js
@@ -64,28 +64,37 @@ export const searchMovies = createAsyncThunk(
   }
 );
 
-
-export const fetchAllDiscover = createAsyncThunk("fetchAllDiscover", async ({ page }) => {
-  const [Allmovies, Alltv] = await Promise.all([
-    axios.get(
-      `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${apiKey}`
-    ),
-    axios.get(
-      `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${apiKey}`
-    ),
-  ]);
-  return {
-    AllMovies: Allmovies.data.results,
-    AllTv: Alltv.data.results,
-    page
-  };
-});
+export const fetchAllDiscover = createAsyncThunk(
+  "fetchAllDiscover",
+  async ({ page }) => {
+    const [Allmovies, Alltv] = await Promise.all([
+      axios.get(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${apiKey}`
+      ),
+      axios.get(
+        `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${apiKey}`
+      ),
+    ]);
+    return {
+      AllMovies: Allmovies.data.results,
+      AllTv: Alltv.data.results,
+      page,
+    };
+  }
+);
 
 const slice = createSlice({
   name: "moviesSlice",
   initialState: {
+    trendingMoviesByDay: [],
+    trendingMoviesByWeek: [],
+    topRatedMovies: [],
+    topRatedTVShows: [],
+    popularMovies: [],
+    popularTVShows: [],
     AllMovies: [],
     AllTv: [],
+    searchResults: [],
     page: 1,
     status: "idle",
     error: null,
@@ -93,17 +102,64 @@ const slice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(fetchTrendingMovies.pending, (state) => {
+        state.status = "Loading...";
+      })
+      .addCase(fetchTrendingMovies.fulfilled, (state, action) => {
+        state.trendingMoviesByDay = action.payload.trendingMoviesByDay;
+        state.trendingMoviesByWeek = action.payload.trendingMoviesByWeek;
+        state.status = "succeeded";
+      })
+      .addCase(fetchTrendingMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(fetchPopularMovies.pending, (state) => {
+        state.status = "Loading...";
+      })
+      .addCase(fetchPopularMovies.fulfilled, (state, action) => {
+        state.popularMovies = action.payload.popularMovies;
+        state.popularTVShows = action.payload.popularTVShows;
+        state.status = "succeeded";
+      })
+      .addCase(fetchPopularMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(fetchTopRatedMovies.pending, (state) => {
+        state.status = "Loading...";
+      })
+      .addCase(fetchTopRatedMovies.fulfilled, (state, action) => {
+        state.topRatedMovies = action.payload.topRatedMovies;
+        state.topRatedTVShows = action.payload.topRatedTVShows;
+        state.status = "succeeded";
+      })
+      .addCase(fetchTopRatedMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(searchMovies.pending, (state) => {
+        state.status = "Loading...";
+      })
+      .addCase(searchMovies.fulfilled, (state, action) => {
+        state.searchResults = action.payload;
+        state.status = "succeeded";
+      })
+      .addCase(searchMovies.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
       .addCase(fetchAllDiscover.pending, (state) => {
         state.status = "Loading...";
       })
       .addCase(fetchAllDiscover.fulfilled, (state, action) => {
-        state.status = "idle";
-        state.AllMovies = [...state.AllMovies, ...action.payload.AllMovies];
-        state.AllTv = [...state.AllTv, ...action.payload.AllTv];
+        state.AllMovies = action.payload.AllMovies;
+        state.AllTv = action.payload.AllTv;
         state.page = action.payload.page;
+        state.status = "succeeded";
       })
       .addCase(fetchAllDiscover.rejected, (state, action) => {
-        state.status = "There is an error";
+        state.status = "failed";
         state.error = action.error.message;
       });
   },
