60c3c46654d4fbff12ca66ab153dd7f41a827e42
harshal0151 <patilharshal0151@gmail.com>
2024-07-17 12:55:58 +0530

    all screen done

--- src/Components/MoviesDisplay.jsx
+++ src/Components/MoviesDisplay.jsx
@@ -22,7 +22,7 @@ function MoviesDisplay({ heading, option1, option2, choice1, choice2 }) {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 1,
+         
           infinite: true,
           dots: false,
          
@@ -32,14 +32,15 @@ function MoviesDisplay({ heading, option1, option2, choice1, choice2 }) {
         breakpoint: 770,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1
+          slidesToScroll: 3,
+         
         }
       },
       {
         breakpoint: 420,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
           dots: false,
         }
       }
